a1002a14d81d25d33df85a34a9b8425d9787ed15
HanYoungsoo <gglife333@gmail.com>
2018-06-30 17:52:06 +0900

    configured redux-persist

--- store/configureStore.js
+++ store/configureStore.js
@@ -4,10 +4,13 @@ import rootReducer from "../reducers"
 import thunk from "redux-thunk"
 import { persistStore, persistReducer } from "redux-persist"
 import storage from "redux-persist/lib/storage"
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2"
 
 const persistConfig = {
   key: "root",
-  storage
+  storage,
+  stateReconciler: autoMergeLevel2,
+  blacklist: ["counter", "visibilityFilter"]
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
